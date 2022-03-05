5f523d7818b480d125ece3f31ed87059fdb7de23
Federico <codepalace@fastmail.se>
2022-03-05 16:18:41 +0100

    Initial Commit

---
+++ PasswordGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PasswordGenerator
+//
+//  Created by Federico on 05/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Federico on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
